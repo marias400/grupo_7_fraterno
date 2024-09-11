1696b4d6632c4675598ce67146db436f980cd1b4
Daniel Zelaya <luisdanielzelaya@gmail.com>
2024-09-11 19:55:39 -0300

    fix edit product CRUD

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,13 +2,15 @@ const datasource = require("../services/inventoryData");
 const inventoryData = require("../services/inventoryData");
 const db = require('../database/models');
 const { Where } = require("sequelize/lib/utils");
+const { where } = require("sequelize");
 
 //vistas administrador:
 
 const adminController = {
   products: null,
 
-  //busca y edita producto
+  //
+  //Envia un producto al formulario de edicion y carga los productos en un selector.
  
   async editPage(req, res) {
 
@@ -30,76 +32,100 @@ const adminController = {
    
   },
 
-  //edita producto
+  //Guardar la edicion de un producto
+  
   async editLogic(req, res) {
-    this.products = await inventoryData.load();
-    const id = req.params.id;
-    const { image } = this.products.find((item) => item.id === id);
+  //  this.products = await inventoryData.load();
+    let { id } = req.params;
+     db.Product.findByPk(id)
+        .then(product=>{
+          if(product === null){
+            console.log("Producto no encontrado!");
+
+          } else {
+            let editedProduct = {
+              name: req.body.name, description: req.body.description,
+              image: req.body.image, category: req.body.category, suitability: req.body.suitability,
+              size: req.body.size, price: req.body.price  
+              // faltaria Stock en la vista (formulario)            
+            }
+            db.Product.update(editedProduct,{where:{id}})
+                .then(resultado=>{
+                  console.log(resultado);
+                  res.redirect(`/admin/products/${id}/edit`);
+                 // res.render("admin/product-editor", { productItem: products});
+                })
+          }
+        })
+
+        
+   // const id = req.params.id;
+  //  const { image } = this.products.find((item) => item.id === id);
 
     // Lógica para determinar la imagen de producto
-    let imageFilePath;
+    // let imageFilePath;
 
-    if (req.file) {
-      // Si se subió una nueva imagen
-      imageFilePath = `/images/products/${req.file.filename}`;
+    // if (req.file) {
+    //   // Si se subió una nueva imagen
+    //   imageFilePath = `/images/products/${req.file.filename}`;
 
-      // Si había una imagen anterior, eliminarla
-      if (image && image !== "" && image !== "/images/products/default.png") {
-        datasource.removeFile(image);
-      }
-    } else {
-      // Si no se subió una nueva imagen
-      if (image && image !== "") {
-        // Mantener la imagen anterior si existe
-        imageFilePath = image;
-      } else {
-        // Si no había imagen anterior, establecer la imagen por defecto
-        imageFilePath = "/images/products/default.png";
-      }
-    }
+    //   // Si había una imagen anterior, eliminarla
+    //   if (image && image !== "" && image !== "/images/products/default.png") {
+    //     datasource.removeFile(image);
+    //   }
+    // } else {
+    //   // Si no se subió una nueva imagen
+    //   if (image && image !== "") {
+    //     // Mantener la imagen anterior si existe
+    //     imageFilePath = image;
+    //   } else {
+    //     // Si no había imagen anterior, establecer la imagen por defecto
+    //     imageFilePath = "/images/products/default.png";
+    //   }
+    // }
 
-    const {
-      name,
-      description,
-      sku,
-      ingredients,
-      category,
-      vegetarian,
-      vegan,
-      celiac,
-      size,
-      price,
-    } = req.body;
+    // const {
+    //   name,
+    //   description,
+    //   sku,
+    //   ingredients,
+    //   category,
+    //   vegetarian,
+    //   vegan,
+    //   celiac,
+    //   size,
+    //   price,
+    // } = req.body;
 
-    let suitability = {
-      vegetarian: vegetarian === "on" || false,
-      vegan: vegan === "on" || false,
-      celiac: celiac === "on" || false,
-    };
+    // let suitability = {
+    //   vegetarian: vegetarian === "on" || false,
+    //   vegan: vegan === "on" || false,
+    //   celiac: celiac === "on" || false,
+    // };
 
-    //modificacion a la "base de datos"
-    const updatedInventory = products.map((toModify) => {
-      if (toModify.id === id) {
-        return {
-          id,
-          name,
-          description,
-          sku,
-          ingredients: ingredients.split(", "),
-          image: imageFilePath,
-          category,
-          suitability,
-          size,
-          price,
-        };
-      } else {
-        return toModify;
-      }
-    });
+    // //modificacion a la "base de datos"
+    // const updatedInventory = products.map((toModify) => {
+    //   if (toModify.id === id) {
+    //     return {
+    //       id,
+    //       name,
+    //       description,
+    //       sku,
+    //       ingredients: ingredients.split(", "),
+    //       image: imageFilePath,
+    //       category,
+    //       suitability,
+    //       size,
+    //       price,
+    //     };
+    //   } else {
+    //     return toModify;
+    //   }
+    // });
 
-    await inventoryData.save(updatedInventory);
+    // await inventoryData.save(updatedInventory);
 
-    res.redirect(`/admin/products/${id}/edit`);
+    // res.redirect(`/admin/products/${id}/edit`);
   },
 
   async createPage(req, res) {
