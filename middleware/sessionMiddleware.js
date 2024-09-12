4fe4bfe578cc463309e90ade80d939ab52dd7b8c
Mariano Arias Simone <nano.arias12@gmail.com>
2024-09-11 23:47:40 -0300

    fix: ahora si se diferencian legitimamente las vistas de administradores vs. usuario

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -1,5 +1,8 @@
 function sessionMiddleware(req, res, next) {
   res.locals.userSession = req.session.user || null;
+  if (req.session.user) {
+    res.locals.userSession.isAdmin = req.session.user.admin;
+  }
   next();
 }
 
