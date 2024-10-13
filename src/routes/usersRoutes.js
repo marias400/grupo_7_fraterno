fd88e9439d195b8b5a43c2e1c5e7937e387565a9
Mariano Arias Simone <nano.arias12@gmail.com>
2024-10-13 19:48:29 -0300

    feat: validaciones en formularios de usuario

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -9,20 +9,22 @@ const fileUploadUsers = require('../services/fileUploadUsers');
 
 router.get("/login", auth.profileAuth, usersController.loginPage);
 router.post("/login", auth.userInctiveAuth, validations.login, areEmailAndPassInDB, usersController.loginSuccesful);
+
 router.get("/register", auth.userInctiveAuth, usersController.registerPage);
 router.post("/register", fileUploadUsers.single('image'), validations.register, usersController.processRegister);
+
 router.get("/profile", auth.userActiveAuth, usersController.profilePage);
 router.post("/profile", auth.userActiveAuth, usersController.userLogout);
 
 router.get("/support", auth.userActiveAuth, usersController.supportPage);
 
 router.get("/profile/info", auth.userActiveAuth, usersController.infoPage);
-router.post("/profile/info", auth.userActiveAuth, usersController.infoUpdate);
+router.post("/profile/info", auth.userActiveAuth, validations.editProfile, usersController.infoUpdate);
 
 router.get("/profile/orders", auth.userActiveAuth, usersController.ordersPage);
 
 router.get("/profile/password", auth.userActiveAuth, usersController.passwordPage);
-router.post("/profile/password", auth.userActiveAuth, usersController.passwordUpdate);
+router.post("/profile/password", auth.userActiveAuth, validations.changePassword, usersController.passwordUpdate);
 
 
 module.exports = router;
