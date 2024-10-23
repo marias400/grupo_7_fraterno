c1f8759e723b84642ef9795fb369b32ac63268c8
PoisonSyringe <gargarense@hotmail.com>
2024-10-23 00:08:39 -0300

    feat: REACT ultimo usuario registrado component

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <UserTab></UserTab>
+      <UserTab/>
     </>
   )
 }
--- dashboard/src/components/UserTab.jsx
+++ dashboard/src/components/UserTab.jsx
@@ -1,16 +1,30 @@
 import { useFetch } from "../hooks/useFetch"
-import UserDetail from "./UserDetail"
 
 export default function UserTab() {
     const { data, isLoading } = useFetch("api/users")
+
+    if (isLoading) {
+        return <p>Cargando...</p>;
+    }
+
+    const user = data?.users.findLast((user) => { return (user) })
+
+    if (!user) {
+        return <p>No hay usuarios registrados</p>;
+    }
+
     return (
-        <>
-            {
-                data?.users.map((user) => {
-                    return (
-                        <UserDetail id={user.id}>{user.firstName}</UserDetail>)
-                })
-            }
-        </>
+        <div className="tab">
+            <div className="tab-title">
+                <h3>Último usuario registrado</h3>
+            </div>
+            <div className="tab-content">
+                <p>Id {user.id}</p>
+                <p>{user.firstName}</p>
+                <p>{user.email}</p>
+                <a>localhost:8000/api/users/{user.id}</a>
+                {/* <p>{user.detail}</p> */}
+            </div>
+        </div>
     )
 }
