86e675a2ddcb13d97b487cada4e3bb1a21cf3020
BiagioliJuanManuel <biagiolijuan@gmail.com>
2024-10-24 01:29:59 -0300

    fix: agregado endpoint usuario por id

--- src/controllers/api/userApi.js
+++ src/controllers/api/userApi.js
@@ -9,6 +9,30 @@ const userApi = {
       });
     });
   },
+
+  userById(req,res){
+    let {id} = req.params
+    db.User.findByPk(id).then((data)=>{
+      if(data !== null){
+        let user = {
+          name: `${data.firstName} ${data.lastName}`,
+          email: data.email,
+          phone: data.phone,
+          address: data.address,
+          image: `localhost:8000${data.image}`
+        }
+        return res.json(user);
+      }else{
+        return res.status(404).json({
+          status: 404,
+          error: "No se encontro usuario asociado al id ingresado "
+        })
+      }
+    }).catch(
+      err => console.log(err)
+    )
+
+  }
 };
 
 module.exports = userApi;
--- src/routes/api/userApi.js
+++ src/routes/api/userApi.js
@@ -4,5 +4,6 @@ const userApi = require("../../controllers/api/userApi");
 
 
 router.get("/users", userApi.userCount)
+router.get("/users/:id", userApi.userById)
 
 module.exports = router;
